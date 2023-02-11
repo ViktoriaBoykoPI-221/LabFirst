e218f2e113eb6200412686fc9130af7f1880efc1
Viktoria <V2018006@yandex.ru>
2023-02-11 21:41:05 +0700

    Добавьте файлы проекта.

---
+++ LabFirst/EcheLabFirst/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Universe
+{
+    class Cometa
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите число х (х >= 100): ");
+            int x = int.Parse(Console.ReadLine());
+            int number = x;
+
+            do
+            {
+                number = number / 10;
+                if (number < 100)
+                {
+                    do
+                    {
+                        number = number - 10;
+                    }
+                    while (number > 10);
+                }
+            }
+            while (number > 100);
+            int n = int.Parse(x.ToString().Remove(1, 1) + number);
+            Console.Write("результат: " + n);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabFirst/LabFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cosmos
+{
+    class Zvesda
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число (a): ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите показатель степени (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            double result = 1;
+
+            for (int index = 0; index < n; ++index)
+            {
+                result *= a;
+            }
+
+            Console.Write("a^n = " + result);
+        }
+    }
+}
